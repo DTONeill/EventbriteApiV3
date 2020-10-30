4a86e20b8f40dba03d1c049c7f52b63a0eb9a94d
egbertnierop <egbert.nierop@adccure.nl>
2020-10-30 14:26:28 +0100

    support organization endpoint

--- EventbriteApiV3/EventbriteRequestBase.cs
+++ EventbriteApiV3/EventbriteRequestBase.cs
@@ -62,5 +62,11 @@ namespace EventbriteApiV3
                 return await sr.ReadToEndAsync();
             }
         }
+        protected  async Task<TextReader> GetStreamResponseAsync()
+        {
+            var request = WebRequest.Create(Url);
+            request.Headers.Add("authorization", $"Bearer {Context.AppKey}");
+            return new StreamReader((await  request.GetResponseAsync()).GetResponseStream());
+        }
     }
 }
---
+++ EventbriteApiV3/Events/EventsOrganizationRequest.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace EventbriteApiV3.Events
+{
+	public class EventsOrganizationRequest: EventbriteRequestBase
+	{
+		private const string Path = "organizations/{0}/events/";
+		public EventsOrganizationRequest(EventbriteContext context, long organizationId, BaseSearchCriterias criterias)
+		: base(string.Format(Path, organizationId), context, criterias.ToNameValueCollection())
+		{
+
+		}
+
+		public async Task<EventsSearchApiResponse> GetResponseAsync()
+		{
+			var response = await GetStreamResponseAsync();
+			var JsonSerializer = new JsonSerializer();
+			var result = JsonSerializer.Deserialize< EventsSearchApiResponse>(new JsonTextReader( response ));
+			return result;
+		}
+	}
+}
