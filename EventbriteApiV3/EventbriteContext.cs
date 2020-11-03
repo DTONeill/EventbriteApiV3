48b83d14b5ca4cba9dac122d1717e3e535577b65
David Tremblay O'Neill <davidtremblay055@gmail.com>
2020-11-02 20:06:31 -0500

    Update EventbriteContext.cs
    Convention: new line between methods.

--- EventbriteApiV3/EventbriteContext.cs
+++ EventbriteApiV3/EventbriteContext.cs
@@ -21,19 +21,22 @@ namespace EventbriteApiV3
         {
             return new EventSearchEventbriteRequest(this, searchCriterias).GetResponse();
         }
+
         public Task<EventsSearchApiResponse> GetEventsAsync(BaseSearchCriterias searchCriterias)
         {
             return (new EventSearchEventbriteRequest(this, searchCriterias)).GetResponseAsync();
         }
+	    
         public Task<EventsSearchApiResponse> GetEventsByOrganization(long organisationId, BaseSearchCriterias searchCriterias)
-		{
+	{
             return (new EventsOrganizationRequest(this, organisationId, searchCriterias)).GetResponseAsync();
-
         }
+
         public AttendeeSearchApiResponse GetAttendees(double eventId, BaseSearchCriterias searchCriterias)
         {
             return new AttendeeSearchEventbriteRequest(this, eventId, searchCriterias).GetResponse();
         }
+
         public Task<AttendeeSearchApiResponse> GetAttendeesAsync(double eventId, BaseSearchCriterias searchCriterias)
         {
             return (new AttendeeSearchEventbriteRequest(this, eventId, searchCriterias)).GetResponseAsync();
