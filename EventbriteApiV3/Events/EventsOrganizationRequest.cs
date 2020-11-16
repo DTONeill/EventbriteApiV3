3faa5e99e62394980a5e5ff2d19d9009255d0835
egbertnierop <egbert.nierop@adccure.nl>
2020-11-16 15:33:27 +0100

    ease on eventbrite

--- EventbriteApiV3/Events/EventsOrganizationRequest.cs
+++ EventbriteApiV3/Events/EventsOrganizationRequest.cs
@@ -6,7 +6,9 @@ namespace EventbriteApiV3.Events
 {
 	public class EventsOrganizationRequest : EventbriteRequestBase
 	{
-		private const string Path = "organizations/{0}/events";
+		//<-- bug in event brite. Postman accepts without but our api
+		//cannot leave the ending slash out
+		private const string Path = "organizations/{0}/events/";
 		private readonly static Lazy<JsonSerializer> JsonSerializerLazy = new Lazy<JsonSerializer>(() => JsonSerializer.CreateDefault( new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore }));
 
 		public EventsOrganizationRequest(EventbriteContext context, long organizationId, BaseSearchCriterias criterias)
